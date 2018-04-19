dc35958dee9244e3e98274f02018b4d615b00dc0
Jorge Zapata Guridi <jorge.zapata@jazg.net>
2018-04-18 20:51:54 -0700

    Super quick test

--- api/AlexaHandler.js
+++ api/AlexaHandler.js
@@ -2,6 +2,7 @@
 
 const Alexa = require('alexa-sdk');
 const fs = require('fs');
+const volumeController = require('./controllers/VolumeController');
 
 exports.handler = function(event, context, callback) {
     const alexa = Alexa.handler(event, context, callback);
@@ -28,9 +29,11 @@ const handlers = {
         let slots = this.event.request.intent.slots;
         if (slots.audioStatus.value === 'increase' || slots.audioStatus.value === 'raise') {
             speech = 'Ok, raising volume by ' + slots.audioLevel.value;
+            volumeController.volumeUp(slots.audioLevel.value);
         }
         else if (slots.audioStatus.value === 'lower') { //|| slots.audioStatus.value === 'decrease'
             speech = 'Ok, lowering volume by ' + slots.audioLevel.value;
+            volumeController.volumeDown(slots.audioLevel.value);
         }
         
         this.emit(':tell', speech);
--- api/controllers/VolumeController.js
+++ api/controllers/VolumeController.js
@@ -3,13 +3,23 @@
 var os = require('child_process');
 
 module.exports = {
-    volumeUp: function (req, res) {
+    volumeUp: function (level) {
         var cmd = 'irsend SEND_ONCE POLAROID_TV KEY_VOLUMEUP';
-        var child = os.spawn(cmd, [], {shell: true});
-        res.send('irsend SEND_ONCE POLAROID_TV KEY_VOLUMEUP');
+        for (var i=0; i<level; i++) {
+            var child = os.spawn(cmd, [], {shell: true});
+            console.log(cmd);
+        }
     },
 
-    volumeDown: function (req, res) {
+    volumeDown: function (level) {
+        var cmd = 'irsend SEND_ONCE POLAROID_TV KEY_VOLUMEDOWN';
+        for (var i=0; i<level; i++) {
+            var child = os.spawn(cmd, [], {shell: true});
+            console.log(cmd);
+        }
+    },
+
+    volumeDown1: function (req, res) {
         var cmd = '/usr/bin/irsend SEND_ONCE POLAROID_TV KEY_VOLUMEDOWN';
         var child = os.execFile(cmd, [], function(err, stdout, stderr) {
             if (err) {
